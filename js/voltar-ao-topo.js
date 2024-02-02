1b0835548152b2d0fc6b4fa4d26d49c7646dd86f
@fmota-dev <fmota.web@gmail.com>
2024-02-02 13:35:02 -0300

    otimizações no site

---
+++ js/voltar-ao-topo.js
@@ -0,0 +1,14 @@
+const btnVoltarAoTopo = document.getElementById("voltar-ao-topo")
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btnVoltarAoTopo.style.display = "block"
+  } else {
+    btnVoltarAoTopo.style.display = "none"
+  }
+})
+
+btnVoltarAoTopo.addEventListener("click", (e) => {
+  e.preventDefault()
+  window.scrollTo({ top: 0, behavior: "smooth" })
+})
