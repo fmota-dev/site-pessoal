d1339432c16c8fba175ee5cbf0efa246dae90def
@fmota-dev <fmota.web@gmail.com>
2024-03-19 11:59:53 -0300

    :iphone: feat: adicionar animação mais rapida em mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,7 +56,7 @@ function initAnimationScroll() {
 	const sections = document.querySelectorAll('.js-scroll');
 
 	if (sections.length) {
-		const windowMetade = window.innerHeight * 0.6;
+		const windowMetade = window.innerWidth > 550 ? window.innerHeight * 0.6 : window.innerHeight * 0.7;
 		function animaScroll() {
 			sections.forEach((section) => {
 				const sectionTop = section.getBoundingClientRect().top;
